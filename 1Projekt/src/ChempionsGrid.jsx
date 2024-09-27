db02d01fe71c6104f0090bda21149f7ae7cd6670
Kamil Gieras <104592814+kamilGie@users.noreply.github.com>
2024-09-28 00:42:43 +0200

    UPDATE

--- 1Projekt/src/ChempionsGrid.jsx
+++ 1Projekt/src/ChempionsGrid.jsx
@@ -1,13 +1,28 @@
 import Card from "./card"
 import propTypes from "prop-types"
 import { ChempionList } from "./ChempionsData"
+import { useEffect, useState } from "react";
 
 function ChempionsGrid(props) {
+	const [FilteredChampions, setFilteredChampions] = useState([...ChempionList]);
+	const [inputFilter, setInputFilter] = useState("");
+
+	useEffect(() => {
+		setFilteredChampions(ChempionList.filter(c => c.name.toLowerCase().includes(inputFilter.toLowerCase())));
+	}, [inputFilter]);
+
+	function SetFilter(e) {
+		setInputFilter(e.target.value);
+	}
+
 	return (
-		<div className="ChampContainer">
-			{ChempionList.map(chemp => (
-				<Card key={chemp.id} name={chemp.name} desc={chemp.desc} img={chemp.img} func={props.func} id={chemp.id} />
-			))}
+		<div>
+			<input value={inputFilter.current} onChange={SetFilter} />
+			<div className="ChampContainer">
+				{FilteredChampions.map(chemp => (
+					<Card key={chemp.id} name={chemp.name} desc={chemp.desc} img={chemp.img} func={props.func} id={chemp.id} />
+				))}
+			</div>
 		</div>
 	);
 }
