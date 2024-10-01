0ae91c45120e9ba944529730e25f4f853213be0f
Kamil Gieras <104592814+kamilGie@users.noreply.github.com>
2024-10-01 07:12:09 +0200

    UPDATE

--- 1Projekt/client/src/App.jsx
+++ 1Projekt/client/src/App.jsx
@@ -1,17 +1,17 @@
 import { useState } from "react";
 import Footer from "./Footer/ Footer.jsx";
-import Header from "./Header/Header.jsx"
+import Header from "./Header/Header.jsx";
 import MainContent from "./MainContent/MainContent.jsx";
 
 function App() {
-	const [nickAndTag, SetNickAndTag] = useState({ nick: "Faker", tag: "eune" })
-	return (
-		<>
-			<Header user={SetNickAndTag} />
-			<MainContent user={nickAndTag} />
-			<Footer />
-		</>
-	);
+  const [nickAndTag, SetNickAndTag] = useState({ nick: "Faker", tag: "eune" });
+  return (
+    <>
+      <Header user={SetNickAndTag} />
+      <MainContent user={nickAndTag} />
+      <Footer />
+    </>
+  );
 }
 
-export default App
+export default App;
--- 1Projekt/client/src/Header/Header.jsx
+++ 1Projekt/client/src/Header/Header.jsx
@@ -4,30 +4,31 @@ import acc from "../assets/acc.svg"
 import { useState } from "react";
 
 function Header(props) {
-	const [tag, setTag] = useState("eune")
-	const [nick, setNick] = useState("Faker")
-	function SubmitUser() {
-		console.log("Submiting user")
-		props.user({ nick: nick, tag: tag })
-	}
-	return (
-		<div className={styles.Header}>
-			<h1 className={styles.Logo}>LOL STATS</h1>
-			<div className={styles.SearchBars}>
-				<input className={styles.nickBar} value={nick} onChange={(e) => { setNick(e.target.value) }} />
-				<input className={styles.tagBar} value={tag} onChange={(e) => { setTag(e.target.value) }} />
-				<button className={styles.tagBar} value={tag} onClick={SubmitUser}> subbmit</button>
-			</div>
+        const [tag, setTag] = useState("eune")
+        const [nick, setNick] = useState("Faker")
+        function SubmitUser() {
+                console.log("Submiting user")
+                props.user({ nick: nick, tag: tag })
+        }
 
-			<img className={styles.AccImg} width="40" height="40" src={acc}>
+        return (
+                <div className={styles.Header}>
+                        <h1 className={styles.Logo}>LOL STATS</h1>
+                        <div className={styles.SearchBars}>
+                                <input className={styles.nickBar} value={nick} onChange={(e) => { setNick(e.target.value) }} />
+                                <input className={styles.tagBar} value={tag} onChange={(e) => { setTag(e.target.value) }} />
+                                <button className={styles.tagBar} value={tag} onClick={SubmitUser}> subbmit</button>
+                        </div>
 
-			</img>
-		</div>
-	);
+                        <img className={styles.AccImg} width="40" height="40" src={acc}>
+
+                        </img>
+                </div>
+        );
 }
 
 Header.propTypes = {
-	user: PropTypes.object,
+        user: PropTypes.object,
 }
 
 export default Header
